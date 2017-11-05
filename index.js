fc3bd327f380a5a81f02833dc2afe15e26baf182
Julieta <julietasbenitez@hotmail.com>
2017-11-05 01:00:36 -0300

    lineal y parabola

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ let mainWindow;
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
-  mainWindow.loadURL(url.format({
+    mainWindow.openDevTools();
+
+    mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
