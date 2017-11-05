1831f328bff8d384e0066986975c387492d6eb9f
utnso <fede.5mentarios@gmail.com>
2017-11-05 20:50:14 -0300

    hiperbolas funcionando, calculos en aproximador mejorado, falta agarrar la cantidad de digitos que se esperan

--- aproximador.js
+++ aproximador.js
@@ -1,52 +1,148 @@
 function aproximacionLineal(valores) {
-    var datos = {
-        x: [],
-        puntos: [],
-        funcion: []
-    };
-    var sumatorias = {
-        x: 0,
-        y: 0,
-        xx: 0,
-        xy: 0
-    };
-    valores.forEach(function(value) {
-        sumatorias.x += value.x;
-        sumatorias.y += value.y;
-        sumatorias.xx += value.x * value.x;
-        sumatorias.xy += value.x * value.y;
-        datos.x.push(value.x);
-        datos.puntos.push(value.y);
-    });
+    var datos = datosAGraficar(valores);
+    var sumatorias = sumatoriaLineal(valores);
     var n = valores.length;
-    var m = ((sumatorias.xy - (sumatorias.x * sumatorias.y) / n) / (sumatorias.xx - (sumatorias.x * sumatorias.x) / n));
-    var b = (sumatorias.y / n - m * (sumatorias.x / n));
-    datos.x.forEach(function(x) {
-        var y = m * x + b;
-        datos.funcion.push(y);
-    });
-    var puntos = {
-        x: datos.x,
-        y: datos.puntos,
-        mode: 'markers',
-        name: 'Puntos tabla'
-    };
-    var funcion = {
-        x: datos.x,
-        y: datos.funcion,
-        type: 'scatter',
-        name: 'Aproximacion funcion'
-    };
-    var aproximacion = {
-        sumatorias: sumatorias,
-        m: m.toFixed(2),
-        b: b.toFixed(2),
-        n: n,
-        funcion: funcion,
-        puntos: puntos
-    };
-    return aproximacion;
+    var m = pendiente(sumatorias,n);
+    var b = altura(sumatorias,n,m);
+    datos = datosDeFuncionLineal(datos,m,b);
+    return aproximacion(sumatorias,m,b,datos);
 };
+
+function sumatoriaLineal(valores){
+  var sumatorias = {
+      x: 0,
+      y: 0,
+      xx: 0,
+      xy: 0
+  };
+  valores.forEach(function(value) {
+      sumatorias.x += value.x;
+      sumatorias.y += value.y;
+      sumatorias.xx += value.x * value.x;
+      sumatorias.xy += value.x * value.y;
+  });
+  return sumatorias;
+};
+
+function datosDeFuncionLineal(datos,m,h){
+  datos.x.forEach(function(x) {
+      var y = m * x + h;
+      datos.funcion.push(y);
+  });
+  return datos;
+};
+
+function aproximacionHiperbolica(valores) {
+    var datos = datosAGraficar(valores);
+    var sumatorias = sumatoriaHiperbolica(valores);
+    var n = valores.length;
+    var m = pendiente(sumatorias,n);
+    var h = altura(sumatorias,n,m);
+    a = 1/m
+    b = h*a
+    datos = datosDeFuncionHiperbolica(datos,a,b);
+    return aproximacion(sumatorias,a,b,datos);
+};
+
+function sumatoriaHiperbolica(valores){
+  var sumatorias = {
+      x: 0,
+      y: 0,
+      xx: 0,
+      xy: 0
+  };
+  valores.forEach(function(value) {
+      sumatorias.x += value.x;
+      sumatorias.y += 1/value.y;
+      sumatorias.xx += value.x * value.x;
+      sumatorias.xy += value.x / value.y;
+  });
+  return sumatorias;
+};
+function datosDeFuncionHiperbolica(datos,a,b){
+  datos.x.forEach(function(x) {
+    if ((x + b) != 0)
+      var y = a / (x + b);
+      datos.funcion.push(y);
+  });
+  return datos;
+};
+
+function aproximacionHiperbolicaSat(valores) {
+    var datos = datosAGraficar(valores);
+    var sumatorias = sumatoriaHiperbolicaSat(valores);
+    var n = valores.length;
+    var m = pendiente(sumatorias,n);
+    var h = altura(sumatorias,n,m);
+    a = 1/h
+    b = m*a
+    datos = datosDeFuncionHiperbolicaSat(datos,a,b);
+    return aproximacion(sumatorias,m,b,datos);
+};
+function sumatoriaHiperbolicaSat(valores){
+  var sumatorias = {
+      x: 0,
+      y: 0,
+      xx: 0,
+      xy: 0
+  };
+  valores.forEach(function(value) {
+      sumatorias.x += 1/value.x;
+      sumatorias.y += 1/value.y;
+      sumatorias.xx += 1/(value.x * value.x);
+      sumatorias.xy += 1/(value.x * value.y);
+  });
+  return sumatorias;
+}
+function datosDeFuncionHiperbolicaSat(datos,a,b){
+  datos.x.forEach(function(x) {
+      var y = a * x /( x + b);
+      datos.funcion.push(y);
+  });
+  return datos;
+}
+
+
+function datosAGraficar(valores){
+  var datos = {
+      x: [],
+      puntos: [],
+      funcion: []
+  };
+  valores.forEach(function(value) {
+      datos.x.push(value.x);
+      datos.puntos.push(value.y);
+  });
+  return datos;
+}
+
+function aproximacion(sumatorias,a,b,datos){
+  return {
+      sumatorias: sumatorias,
+      a: a.toFixed(2),
+      b: b.toFixed(2),
+      funcion: {
+          x: datos.x,
+          y: datos.funcion,
+          type: 'scatter',
+          name: 'Aproximacion funcion'
+      },
+      puntos: {
+          x: datos.x,
+          y: datos.puntos,
+          mode: 'markers',
+          name: 'Puntos tabla'
+      }
+  };
+}
+
+function pendiente(sumatorias,n){
+  return ((sumatorias.xy - (sumatorias.x * sumatorias.y) / n) / (sumatorias.xx - (sumatorias.x * sumatorias.x) / n));
+}
+
+function altura(sumatorias,n,m){
+  return (sumatorias.y / n - m * (sumatorias.x / n));
+}
 
 function arrayMax(array) {
     return array.reduce(function(a, b) {
@@ -61,5 +157,7 @@ function arrayMin(array) {
 }
 
 module.exports = {
-    aproximacionLineal: aproximacionLineal
+  aproximacionLineal: aproximacionLineal,
+  aproximacionHiperbolica: aproximacionHiperbolica,
+  aproximacionHiperbolicaSat: aproximacionHiperbolicaSat
 };
--- recta.js
+++ recta.js
@@ -1,6 +1,5 @@
 require('./jquery-3.2.1.min');
 var aproximador = require('./aproximador');
-
 $( document ).ready(function() {
     $("#masCampos").click(function() {
         $("#tabla").append("<tr><td><input type=\"number\" id=\"valorx\" class=\"casillero\"></td>" +
@@ -15,16 +14,15 @@ $("#Agregar").click(function(){
 });
 
 $("#Generar").click(function(){
-    valores = [];
+  var valores = [];
     $("#tablaResultados").html("<tr><th></th><th>X</th><th>Y</th><th>X^2</th><th>YX</th></tr>");
     $('#tablaValores tr').each(function() {
         x=parseFloat($(this).find(".x").val());
         y=parseFloat($(this).find(".y").val());
-        if($.isNumeric(x)&&$.isNumeric(y)){
-            valores.push({
-                x: x,
-                y: y
-            });
+        if($.isNumeric(x)
+            && $.isNumeric(y)){
+
+            valores.push({x: x,y: y});
         }
     });
 
@@ -38,7 +36,7 @@ $("#Generar").click(function(){
     $("#tablaResultados").append("<tr><td>Σ</td><td>" + aproximacion.sumatorias.x + "</td>" + "<td>" + aproximacion.sumatorias.y + "</td>" +
         "<td>" + aproximacion.sumatorias.xx + "</td>" + "<td>" + aproximacion.sumatorias.xy + "</td></tr>");
 
-    $("#resultado").html("Funcion aproximada: Y=(" + aproximacion.m + ")*X +" + " (" + aproximacion.b + ")");
+    $("#resultado").html("Funcion aproximada: Y=(" + aproximacion.a + ")*X +" + " (" + aproximacion.b + ")");
 
     Plotly.newPlot('myDiv', [aproximacion.funcion, aproximacion.puntos]);
 });
